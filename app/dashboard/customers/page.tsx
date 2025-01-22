cbcb42cd7ef9b2b6b81a06b7f5e874b6596d53e4
Juan Ignacio <juanirmariani@gmail.com>
2025-01-22 17:42:01 -0300

    Course Completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,18 +1,25 @@
-import type { Metadata } from 'next'
-
-import { fetchFilteredCustomers } from '@/app/lib/data'
-import CustomersTable from '@/app/ui/customers/table'
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: 'Customers',
-}
+};
 
-export default async function Page({ searchParams}: { searchParams?: { query?: string; page?: string } }) {
-    const query = searchParams?.query || ''
-    const customers = await fetchFilteredCustomers(query)
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+
     return (
         <main>
             <CustomersTable customers={customers} />
         </main>
-    )
+    );
 }
